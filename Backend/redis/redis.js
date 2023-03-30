7e96ac32caef0ff93ecd7812fb44437bb6056282
Akashfw <shuklaakash30278@gmail.com>
2023-03-30 10:55:37 +0530

    day3-akash

---
+++ Backend/redis/redis.js
@@ -0,0 +1,20 @@
+const redis= require("redis");
+require("dotenv").config();
+
+const client = redis.createClient({
+    url:`redis://default:${process.env.pass}@redis-19330.c212.ap-south-1-1.ec2.cloud.redislabs.com:19330`
+  })
+  
+    client.on("error", (err) => console.log(err, "ERROR in REDIS"));
+  
+     client.on("ready",()=>{
+         console.log("connected to redis")
+     });
+
+
+     
+
+
+     module.exports={
+         client
+     }
