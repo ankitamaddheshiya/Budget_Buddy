ad0e825c6c48b02b7795e9881fa453ec1e9cca59
akshaychavan010101 <akshaychavan010101@gmail.com>
2023-03-30 22:07:23 +0530

    user options details

--- Frontend/Scripts/User-profile.js
+++ Frontend/Scripts/User-profile.js
@@ -1,5 +1,5 @@
-// user options click event and mouseover event
-
+// user options click event
+let Page_name_heading = document.getElementById("Page-name-heading")
 const userOptions = document.querySelectorAll(".user-options ul li");
 
 const userAccountPage = `
@@ -7,8 +7,8 @@ const userAccountPage = `
             <div class="left-profile-section">
             <div class="profile-photo-and-info-div">
                 <div>
-                <img src="../images/github.png" alt="" />
-                <h2>Name</h2>
+                <img src="../images/user.png" alt="" />
+                <h3>Name</h3>
                 </div>
                 <div>
                 <div><p>Designation</p></div>
@@ -39,24 +39,24 @@ const userAccountPage = `
             <div class="right-profile-section">
             <div class="user-details-div">
                 <div>
-                <h3>First Name:</h3>
-                <h3>Akshay</h3>
+                <h4>First Name</h4>
+                <h4>Akshay</h4>
                 </div>
                 <div>
-                <h3>Last Name:</h3>
-                <h3>Chavan</h3>
+                <h4>Last Name</h4>
+                <h4>Chavan</h4>
                 </div>
                 <div>
-                <h3>Email</h3>
-                <h3>akshaychavan010101@gmail.com</h3>
+                <h4>Email</h4>
+                <h4>akshaychavan010101@gmail.com</h4>
                 </div>
                 <div>
-                <h3>Phone Number :</h3>
-                <h3>9373849068</h3>
+                <h4>Phone Number</h4>
+                <h4>9373849068</h4>
                 </div>
                 <div>
-                <h3>Adresss:</h3>
-                <h3>Pune</h3>
+                <h4>Adresss</h4>
+                <h4>Pune</h4>
                 </div>
             </div>
 
@@ -78,20 +78,265 @@ const userAccountPage = `
             </div>
             `;
 
-userOptions.forEach((option) => {
-  let various_display_div = document.querySelector(".various-display-div");
+const userIncomePage = `<h1>Make the layout of user income and append it here</h1>`;
+const userExpensePage = `<h1>Make the layout of user expense and append it here</h1>`;
+const userBudgetPage = `<h1>Make the layout of user budget and append it here</h1>`;
+
+// user option click event
+let various_display_div = document.querySelector(".various-display-div");
+
+const accountOption = (event) => {
   various_display_div.innerHTML = null;
-  option.addEventListener("click", (event) => {
-    userOptions.forEach((option) => {
-      if (option.children[1].innerText == "Account") {
-        various_display_div.innerHTML = userAccountPage;
+  Page_name_heading.innerHTML = "Account"
+  userOptionsColor();
+  if (event.target.children[1] == undefined) {
+    event.target.parentElement.style.backgroundColor = "#ffffff";
+    event.target.parentElement.children[1].style.color = "#306DB3";
+    event.target.parentElement.children[1].style.fontWeight = "bold";
+  } else {
+    event.target.style.backgroundColor = "#ffffff";
+    event.target.children[1].style.color = "#306DB3";
+    event.target.children[1].style.fontWeight = "bold";
+  }
+  various_display_div.innerHTML = userAccountPage;
+};
+const incomeOption = (event) => {
+  various_display_div.innerHTML = null;
+  Page_name_heading.innerHTML = "Income"
+  userOptionsColor();
+  if (event.target.children[1] == undefined) {
+    event.target.parentElement.style.backgroundColor = "#ffffff";
+    event.target.parentElement.children[1].style.color = "#306DB3";
+    event.target.parentElement.children[1].style.fontWeight = "bold";
+  } else {
+    event.target.style.backgroundColor = "#ffffff";
+    event.target.children[1].style.color = "#306DB3";
+    event.target.children[1].style.fontWeight = "bold";
+  }
+  various_display_div.innerHTML = userIncomePage;
+};
+const expenseOption = (event) => {
+  various_display_div.innerHTML = null;
+  Page_name_heading.innerHTML = "Expense"
+  userOptionsColor();
+  if (event.target.children[1] == undefined) {
+    event.target.parentElement.style.backgroundColor = "#ffffff";
+    event.target.parentElement.children[1].style.color = "#306DB3";
+    event.target.parentElement.children[1].style.fontWeight = "bold";
+  } else {
+    event.target.style.backgroundColor = "#ffffff";
+    event.target.children[1].style.color = "#306DB3";
+    event.target.children[1].style.fontWeight = "bold";
+  }
+  various_display_div.innerHTML = userExpensePage;
+};
+const budgetOption = (event) => {
+  various_display_div.innerHTML = null;
+  Page_name_heading.innerHTML = "Budget"
+  userOptionsColor();
+  if (event.target.children[1] == undefined) {
+    event.target.parentElement.style.backgroundColor = "#ffffff";
+    event.target.parentElement.children[1].style.color = "#306DB3";
+    event.target.parentElement.children[1].style.fontWeight = "bold";
+  } else {
+    event.target.style.backgroundColor = "#ffffff";
+    event.target.children[1].style.color = "#306DB3";
+    event.target.children[1].style.fontWeight = "bold";
+  }
+  various_display_div.innerHTML = userBudgetPage;
+};
+
+// user history page
+const userHistoryPage = `  
+      <div class="history-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Transaction</th>
+            <th>Type</th>
+            <th>Date</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody id="tbody-append-history">
+            <tr>
+                <td>1</td>
+                <td>Deposit</td>
+                <td>2021-01-01</td>
+                <td>1000</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Withdraw</td>
+                <td>2021-01-02</td>
+                <td>500</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Deposit</td>
+                <td>2021-01-03</td>
+                <td>1000</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Withdraw</td>
+                <td>2021-01-04</td>
+                <td>500</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>Deposit</td>
+                <td>2021-01-05</td>
+                <td>1000</td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>Withdraw</td>
+                <td>2021-01-06</td>
+                <td>500</td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td>Deposit</td>
+                <td>2021-01-07</td>
+                <td>1000</td>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td>Withdraw</td>
+                <td>2021-01-08</td>
+                <td>500</td>
+            </tr>
+            <tr>
+                <td>9</td>
+                <td>Deposit</td>
+                <td>2021-01-09</td>
+                <td>1000</td>
+            </tr>
+            <tr>
+                <td>10</td>
+                <td>Withdraw</td>
+                <td>2021-01-10</td>
+                <td>500</td>
+            </tr>
+            <tr>
+                <td>11</td>
+                <td>Deposit</td>
+                <td>2021-01-11</td>
+                <td>1000</td>
+            </tr>
+        </tbody>
+      </table>
+      </div>
+`;
+const historyOption = (event) => {
+  various_display_div.innerHTML = null;
+  Page_name_heading.innerHTML = "History"
+  userOptionsColor();
+  if (event.target.children[1] == undefined) {
+    event.target.parentElement.style.backgroundColor = "#ffffff";
+    event.target.parentElement.children[1].style.color = "#306DB3";
+    event.target.parentElement.children[1].style.fontWeight = "bold";
+  } else {
+    event.target.style.backgroundColor = "#ffffff";
+    event.target.children[1].style.color = "#306DB3";
+    event.target.children[1].style.fontWeight = "bold";
+  }
+  various_display_div.innerHTML = userHistoryPage;
+
+  // history fetch request
+  const historyfun = async () => {
+    let loader = `<div class="loading-container"><div class="loader"></div></div>`;
+    various_display_div.innerHTML = loader;
+    try {
+      let history = await fetch(
+        "https://periwinkle-catfish-cuff.cyclic.app/history",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      let historyData = await history.json();
+      console.log(historyData);
+      if (historyData.status == 200) {
+        let tbody = document.getElementById("tbody-append-history");
+        tbody.innerHTML = null;
+        historyData.data.forEach((element) => {
+          let tr = document.createElement("tr");
+          tr.innerHTML = `<td>${element.transaction}</td>
+          <td>${element.type}</td>
+          <td>${element.date}</td>
+          <td>${element.amount}</td>`;
+          tbody.appendChild(tr);
+        });
+      } else {
+        alert("Something went wrong, please try again");
       }
-      option.style.backgroundColor = "#306DB3";
-      option.children[1].style.color = "#ffffff";
-      option.children[1].style.fontWeight = "normal";
-    });
-    option.style.backgroundColor = "#ffffff";
-    option.children[1].style.color = "#306DB3";
-    option.children[1].style.fontWeight = "bold";
+    } catch (error) {
+      various_display_div.innerHTML = userHistoryPage;
+    }
+  }
+  historyfun();
+};
+
+// logout option fetch request;
+const logoutfun = async () => {
+  let loader = `<div class="loading-container"><div class="loader"></div></div>`;
+  various_display_div.innerHTML = loader;
+  try {
+    let logout = await fetch(
+      "https://periwinkle-catfish-cuff.cyclic.app/logout",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    let logoutData = await logout.json();
+
+    if (logoutData.status == 200) {
+      // various_display_div.innerHTML = defaultPage;
+      window.location.assign("Login.html");
+    } else {
+      alert("Something went wrong, please try again");
+    }
+  } catch (error) {
+    various_display_div.innerHTML = userLogoutPage;
+    alert(error.message);
+  }
+};
+
+const userLogoutPage = `
+        <div class=logout-layout>
+           <i class="fa-sharp fa-regular fa-circle-xmark"></i><button onclick="logoutfun()">Logout</button> 
+        </div>
+`;
+const logoutOption = (event) => {
+  Page_name_heading.innerHTML = "Logout"
+  userOptionsColor();
+  if (event.target.children[1] == undefined) {
+    event.target.parentElement.style.backgroundColor = "#ffffff";
+    event.target.parentElement.children[1].style.color = "#306DB3";
+    event.target.parentElement.children[1].style.fontWeight = "bold";
+  } else {
+    event.target.style.backgroundColor = "#ffffff";
+    event.target.children[1].style.color = "#306DB3";
+    event.target.children[1].style.fontWeight = "bold";
+  }
+  various_display_div.innerHTML = userLogoutPage;
+};
+
+// to retrive the user options background colors
+const userOptionsColor = () => {
+  let userOptions = document.querySelectorAll(".user-options ul li");
+  userOptions.forEach((option) => {
+    option.style.backgroundColor = "#306DB3";
+    option.children[1].style.color = "#ffffff";
+    option.children[1].style.fontWeight = "normal";
   });
-});
+};
