782560f2a0fb3bf5315595fa46c23ee8cb5775c9
Snehasish Ghosh Dastidar <kuttulr1996@gmail.com>
2023-04-02 02:24:15 +0530

    day-5 Snehasish

---
+++ Frontend/Scripts/income.js
@@ -0,0 +1,50 @@
+
+// const income_search_btn = document.getElementById("income_search_btn")
+// income_search_btn.addEventListener("click", ()=>{
+//     let form_date = document.getElementById('form_date').value
+//     let to_date = document.getElementById('to_date').value
+
+//     alert (`${form_date} - ${to_date}`)
+// })
+
+
+
+
+
+let add_income_form = document.getElementById("add_income_form")
+let add_income_btn = document.getElementById("add_income_btn")
+add_income_btn.addEventListener("click", ()=>{
+    add_income_form.classList.add("open-popup")
+    // add_income_form.style.visibility = "visible"
+})
+let income_form = document.getElementById("income_form")
+income_form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    add_income_form.classList.remove("open-popup")
+    alert("Income Adding Successful")
+    let input_income_name = document.getElementById("input_income_name")
+    let input_income_type = document.getElementById("input_income_type")
+    let input_income_amount = document.getElementById('input_income_amount')
+
+    console.log(input_income_name.value, input_income_type.value, input_income_amount.value)
+})
+
+let edit_form_popup_div = document.getElementById("edit_form_popup_div")
+const editbtnFun = ()=>{
+    edit_form_popup_div.classList.add("open-popup")
+}
+const dontedit = ()=>{
+    edit_form_popup_div.classList.remove("open-popup")
+}
+
+let add_exp_form = document.querySelector("#add_exp_form")
+let add_exp_btn = document.getElementById("add_exp_btn")
+add_exp_btn.addEventListener("click", ()=>{
+    add_exp_form.classList.add("open-popup")
+})
+let exp_form = document.getElementById("exp_form")
+exp_form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    add_exp_form.classList.remove("open-popup")
+    alert("Expense Added successfully")
+})
